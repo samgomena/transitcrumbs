0f26fa7442903f7e5b73418f5e03a8254086bafd
samgomena <samgomena@protonmail.com>
2020-04-03 00:49:11 -0700

    feat: Start answering questions about this data

---
+++ data_processing/python/main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+from glob import glob
+
+# Grab all tsv data from `data` directory
+ALL_DATA_FILES = glob("../data/**/*.tsv")
+
+
+def load_data():
+    # Taken and/or modified from https://stackoverflow.com/a/21232849/4668680
+    return pd.concat((pd.read_csv(_file, sep="\t") for _file in ALL_DATA_FILES))
+
+
+if __name__ == "__main__":
+    df = load_data()
+
+    q1 = df.shape
+    q2 = len(df["VEHICLE_ID"].unique())
+    print(q2)
