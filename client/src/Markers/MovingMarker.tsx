df732170be215ce15429121f10f2674e3c154e04
samgomena <samgomena@protonmail.com>
2020-05-21 01:39:24 -0700

    feat: Port moving marker to react

--- client/src/Markers/MovingMarker.tsx
+++ client/src/Markers/MovingMarker.tsx
@@ -1,9 +1,41 @@
-import React from "react";
+import { useEffect } from "react";
+import * as L from "leaflet";
+import { MapLayerProps, useLeaflet } from "react-leaflet";
 
-import BusMarker from "./Marker";
+import _MovingMarker from "./moving_marker";
+// import BusMarker from "./Marker";
 
-interface MovingMarkerProps {
-  positions: L.LatLng[];
-}
+type MovingMarkerProps = {
+  positions: L.LatLngTuple[];
+  durations?: number[];
+  // MapLayerProps
+  icon?: L.Icon | L.DivIcon;
+  draggable?: boolean;
+  opacity?: number;
+  // position: LatLngExpression;
+  // duration: number;
+  keepAtCenter?: boolean;
+  zIndexOffset?: number;
+} & MapLayerProps;
 
-const MovingMarker = ({ positions }: MovingMarkerProps) => {};
+const MovingMarker = (props: MovingMarkerProps) => {
+  const { map } = useLeaflet();
+  // Short circuit if the map's unavailable
+  if (map === undefined) return null;
+
+  useEffect(() => {
+    const marker = new _MovingMarker(props.positions, 30000);
+    marker.once("click", () => {
+      marker.start();
+      marker.closePopup();
+      marker.unbindPopup();
+      marker.on("click", () =>
+        marker.isRunning() ? marker.pause() : marker.start()
+      );
+    });
+    marker.addTo(map);
+  });
+  return null;
+};
+
+export default MovingMarker;
