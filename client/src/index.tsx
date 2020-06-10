195acffd23934c02c0b78a3e3c8a4d7d5d9cf421
samgomena <samgomena@protonmail.com>
2020-06-09 19:30:08 -0700

    feat: Add apollo provider and state helper functions

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { ApolloProvider } from "@apollo/react-hooks";
 
@@ -7,22 +7,41 @@ import Pane from "react-split-pane/lib/Pane";
 
 import { client } from "./api";
 import Map from "./Map/Map";
-import { FilterForm, Breadcrumbs } from "./Search/Search";
+import { Routes, Trips } from "./Search/Search";
 
 import "leaflet/dist/leaflet.css";
 import "./index.css";
 
 function App() {
+  const [route, setRoute] = useState<number | null>(null);
+  const [date, setDate] = useState<Date | null>(null);
+  const [vehicle, setVehicle] = useState<number | null>(null);
+
+  console.log(`route: ${route}, date: ${date}, vehicle: ${vehicle}`);
   return (
     <ApolloProvider client={client}>
       <SplitPane split="vertical" primary="second">
         <Pane initialSize="20%" minSize="10%">
-          <h2>Search</h2>
-          <FilterForm />
-          <Breadcrumbs />
+          <div
+            style={{
+              display: "flex",
+              margin: "0.75rem",
+              flexDirection: "column",
+            }}
+          >
+            <h2>Search</h2>
+            <Routes setRoute={setRoute} />
+            <Trips
+              route_number={route}
+              date={date}
+              setDate={setDate}
+              vehicle={vehicle}
+              setVehicle={setVehicle}
+            />
+          </div>
         </Pane>
         <Pane initialSize="80%" minSize="50%" maxSize="100%">
-          <Map />
+          <Map date={date} vehicle={vehicle} />
         </Pane>
       </SplitPane>
     </ApolloProvider>
