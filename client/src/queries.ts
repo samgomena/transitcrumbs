c972d937ad214da61489a7dedcc0e8452358495c
samgomena <samgomena@protonmail.com>
2020-06-10 21:05:16 -0700

    feat: Add additional queries used by search component; remove unused map.ts

--- client/src/queries.ts
+++ client/src/queries.ts
@@ -1,18 +1,101 @@
 import gql from "graphql-tag";
-import { useQuery } from "@apollo/react-hooks";
 
-export const DISTINCT_DAYS = gql`
-  query {
-    breadcrumbs(distinct_on: opd_date) {
-      opd_date
+export const ROUTES = gql`
+  query getRoutes {
+    gtfs_routes {
+      route_id
+      route_long_name
+      route_short_name
     }
   }
 `;
 
-export const DISTINCT_VEHICLES = gql`
-  query {
-    breadcrumbs(distinct_on: vehicle_id) {
-      vehicle_id
+// export const AVL_CAD_DATES = gql`
+//   query getAvlDates {
+//     cad_avl_trips(distinct_on: service_date, order_by: { service_date: asc }) {
+//       service_date
+//     }
+//   }
+// `;
+
+export const AVL_CAD_DATES = gql`
+  query getAvlDatesFromRoute($route_number: Int) {
+    cad_avl_trips(
+      distinct_on: service_date
+      order_by: { service_date: asc }
+      where: { route_number: { _eq: $route_number } }
+    ) {
+      service_date
+    }
+  }
+`;
+
+export const AVL_CAD_VEHICLES_FROM_ROUTE_AND_DATE = gql`
+  query getAvlVehiclesFromRouteAndDay($route_number: Int, $service_date: date) {
+    cad_avl_trips(
+      where: {
+        route_number: { _eq: $route_number }
+        service_date: { _eq: $service_date }
+      }
+      distinct_on: vehicle_number
+    ) {
+      vehicle_number
+    }
+  }
+`;
+
+export const AVL_CAD_TRIPS_FROM_ROUTE_AND_DATE = gql`
+  query getAvlTripsFromRoute($route_number: Int, $service_date: date) {
+    cad_avl_trips(
+      where: {
+        route_number: { _eq: $route_number }
+        service_date: { _eq: $service_date }
+      }
+      distinct_on: trip_number
+    ) {
+      trip_number
+      vehicle_number
+      block
+    }
+  }
+`;
+
+export const AVL_CAD_TRIP_FROM_ROUTE = gql`
+  query getAvlTripsFromRoute($route_number: Int) {
+    cad_avl_trips(
+      where: { route_number: { _eq: $route_number } }
+      distinct_on: trip_number
+    ) {
+      trip_number
+      service_date
+      block
+      vehicle_number
+    }
+  }
+`;
+
+export const GET_BREADCRUMBS_FROM_VEHICLE_AND_DATE = gql`
+  query getBreadcrumbsFromVehicleAndDate($vehicle_id: Int, $opd_date: date) {
+    unique_trips: breadcrumbs(
+      where: { vehicle_id: { _eq: $vehicle_id }, opd_date: { _eq: $opd_date } }
+      distinct_on: event_no_trip
+    ) {
+      event_no_trip
+    }
+    timestamps: breadcrumbs(
+      where: { vehicle_id: { _eq: $vehicle_id }, opd_date: { _eq: $opd_date } }
+      order_by: { act_time: asc }
+    ) {
+      act_time
+    }
+    breadcrumbs: breadcrumbs(
+      where: { vehicle_id: { _eq: $vehicle_id }, opd_date: { _eq: $opd_date } }
+      order_by: { act_time: asc, event_no_trip: asc }
+    ) {
+      lat
+      lon
+      act_time
+      event_no_trip
     }
   }
 `;
@@ -25,17 +108,29 @@ query {
   }
 `;
 
+export const BREADCRUMBS_WHERE = gql`
+  query($route_number: Int) {
+    breadcrumbs(where: { event_no_trip: { _eq: $route_number } }) {
+      lat
+      lon
+    }
+  }
+`;
+
 export const BREADCRUMBS = gql`
   query {
     breadcrumbs(
-      where: { opd_date: { _eq: "2020-03-16" }, vehicle_id: { _eq: 6012 } }
-      order_by: { meters: desc, act_time: desc }
+      where: {
+        opd_date: { _eq: "2020-03-19" }
+        event_no_trip: { _eq: 153914212 }
+      }
+      order_by: { meters: asc, act_time: asc }
     ) {
       lat
       lon
-      act_time
-      meters
-      id
+      # act_time
+      # meters
+      # id
     }
   }
 `;
