5cfe0fdc8d635ed6a7801e4f34ecb4f32cc73866
samgomena <samgomena@protonmail.com>
2020-06-30 23:06:15 -0700

    feat: Whoops also part of move to reducer instead of multi-state

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import { render } from "react-dom";
 import { ApolloProvider } from "@apollo/react-hooks";
 
@@ -12,11 +12,58 @@ import { Routes, Breakdown } from "./Search/Search";
 import "leaflet/dist/leaflet.css";
 import "./index.css";
 
+export interface appState {
+  route: number | null;
+  date: Date | string | null;
+  vehicle: number | null;
+  trips: Array<number> | null;
+}
+
+export enum appActionType {
+  SET_ROUTE,
+  SET_DATE,
+  SET_VEHICLE,
+  SET_TRIPS,
+}
+
+type appActionPayload = any;
+
+interface appActions {
+  type: appActionType;
+  payload: appActionPayload;
+}
+
+const initialState: appState = {
+  route: null,
+  date: null,
+  vehicle: null,
+  trips: [],
+};
+
+function reducer(state: appState, action: appActions) {
+  console.log(state, action);
+  const { SET_ROUTE, SET_DATE, SET_VEHICLE, SET_TRIPS } = appActionType;
+  switch (action.type) {
+    case SET_ROUTE:
+      return { ...state, route: action.payload.route };
+    case SET_DATE:
+      return { ...state, date: action.payload.date };
+    case SET_VEHICLE:
+      return { ...state, vehicle: action.payload.vehicle };
+    case SET_TRIPS:
+      return { ...state, trips: action.payload.trips };
+    default:
+      throw new Error();
+  }
+}
+
 function App() {
   const [route, setRoute] = useState<number | null>(null);
   const [date, setDate] = useState<Date | null>(null);
   const [vehicle, setVehicle] = useState<number | null>(null);
-  const [trips, setTrips] = useState<number | null>(null);
+  const [trips, setTrips] = useState<Array<number> | null>(null);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   console.log(`route: ${route}, date: ${date}, vehicle: ${vehicle}`);
   return (
@@ -31,8 +78,10 @@ function App() {
             }}
           >
             <h2>Search</h2>
-            <Routes setRoute={setRoute} />
+            <Routes state={state} dispatch={dispatch} setRoute={setRoute} />
             <Breakdown
+              state={state}
+              dispatch={dispatch}
               route_number={route}
               date={date}
               setDate={setDate}
@@ -44,7 +93,7 @@ function App() {
           </div>
         </Pane>
         <Pane initialSize="80%" minSize="50%" maxSize="100%">
-          <Map date={date} vehicle={vehicle} trips={trips} />
+          <Map state={state} date={date} vehicle={vehicle} trips={trips} />
         </Pane>
       </SplitPane>
     </ApolloProvider>
