2e0c688a7b6c0ecaad6e80deca606c59971894e2
samgomena <samgomena@protonmail.com>
2020-07-15 21:11:46 -0700

    refac: Be a little smarter about unpacking and using props

--- client/src/Polylines/Hotline.tsx
+++ client/src/Polylines/Hotline.tsx
@@ -14,28 +14,24 @@ type Props = {
 } & PolylineProps;
 
 class Hotline extends Path<PolylineProps, L.Polyline> {
-  createLeafletElement(props: Props) {
-    const positions = props.breadcrumbs.map(({ lat, lon, act_time }, idx) => {
-      if (idx === props.breadcrumbs.length - 1) {
+  createLeafletElement({ breadcrumbs, leaflet }: Props) {
+    const positions = breadcrumbs.map(({ lat, lon, act_time }, idx) => {
+      // Set last LatLng 'heat' to 0 as it probably doesn't matter
+      if (idx === breadcrumbs.length - 1) {
         return [lat, lon, 0];
       }
 
       return [
         lat,
         lon,
-        distance(
-          lat,
-          lon,
-          props.breadcrumbs[idx + 1].lat,
-          props.breadcrumbs[idx + 1].lon
-        ) *
+        distance(lat, lon, breadcrumbs[idx + 1].lat, breadcrumbs[idx + 1].lon) *
           (act_time / 60 / 60),
       ];
     });
     // @ts-ignore: This is egregious and I hate it; I'll be damned if it doesn't work
     return L.hotline(positions, {
       outlineWidth: 0,
-    }).addTo(props.leaflet?.map);
+    }).addTo(leaflet?.map);
   }
 
   updateLeafletElement(fromProps: Props, toProps: Props) {
